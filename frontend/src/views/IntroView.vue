599e969ed35ae3242f976654ebf934ce08b5f1ea
Fabinatix97 <fabian@fabi-online.de>
2026-02-20 10:23:33 +0100

    Simplify layout and styling

--- frontend/src/views/IntroView.vue
+++ frontend/src/views/IntroView.vue
@@ -1,31 +1,27 @@
 <template>
-  <div class="min-h-screen flex justify-center items-center p-6">
-    <main>
-      <div class="flex justify-center my-4">
-        <h1>F1-Wette</h1>
+  <div class="min-h-screen flex flex-col justify-center items-center p-6">
+    <main class="flex flex-col items-center">
+      <h1 class="text-center my-4">F1-Wette</h1>
+
+      <div class="flex flex-col items-center font-extrabold my-2">
+        <span class="text-9xl text-slate-100">20</span>
+        <span class="text-9xl text-[#E10600]">26</span>
       </div>
 
-      <div class="flex justify-center my-2">
-        <div class="flex flex-col font-extrabold items-center">
-          <div class="text-9xl text-slate-100">20</div>
-          <div class="text-9xl text-[#E10600]">26</div>
-        </div>
-      </div>
-
-      <div class="flex justify-center mt-20 mb-2">
-        <div class="flex flex-col space-y-6">
-          <div class="flex bg-[#E10600] py-4 px-8 rounded-full justify-center">
-            <RouterLink to="/explanation" class="font-bold text-slate-100"
-              >Wette abgeben</RouterLink
-            >
-          </div>
-          <div class="flex bg-[#5B5B92] py-4 px-8 rounded-full justify-center">
-            <RouterLink to="/results" class="font-bold text-slate-100"
-              >Ergebnisse ansehen</RouterLink
-            >
-          </div>
-        </div>
-      </div>
+      <nav class="flex flex-col gap-6 mt-20">
+        <RouterLink
+          to="/explanation"
+          class="flex justify-center py-4 px-8 rounded-full font-bold text-slate-100 bg-[#E10600] hover:bg-[#F11610] transition-colors"
+        >
+          Wette abgeben
+        </RouterLink>
+        <RouterLink
+          to="/results"
+          class="flex justify-center py-4 px-8 rounded-full font-bold text-slate-100 bg-[#5B5B92] hover:bg-[#6B6BA2] transition-colors"
+        >
+          Ergebnisse ansehen
+        </RouterLink>
+      </nav>
     </main>
   </div>
 </template>
